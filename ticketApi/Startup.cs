63a0c8b66b1febb9b482b52f4e137b1a81ee8619
tomkf <thomaskeithferris@gmail.com>
2020-02-16 19:54:14 -0800

    swagger UI on localhost: 5001

--- ticketApi/Startup.cs
+++ ticketApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using ticketApi.Models.Tickets;
 
 namespace ticketApi
@@ -43,6 +44,10 @@ namespace ticketApi
                     });
             });
 
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ticket API", Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -61,6 +66,13 @@ namespace ticketApi
 
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
